587e8d886c4bec1e8fb02af099bc5a00847a7245
CSE-pebble <CSE.pebble@gmail.com>
2022-09-06 11:06:16 +0900

    Add greeting function

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault(); //event의 기본동작을 막아주는 코드
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value; //사용자 입력값 받아서 변수에 저장
-  loginForm.classList.add("hidden");
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit); //submit event
