5444fab2b613e7b20b9bbe8c8c6e7f087b080c53
CSE-pebble <CSE.pebble@gmail.com>
2022-09-06 11:39:54 +0900

    Add username to local storage

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
   event.preventDefault(); //event의 기본동작을 막아주는 코드
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value; //사용자 입력값 받아서 변수에 저장
+  localStorage.setItem("username", username);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
