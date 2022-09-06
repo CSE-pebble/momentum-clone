825bb6537a05cdc3537eb5096e1b1c00c3a8496b
CSE-pebble <CSE.pebble@gmail.com>
2022-09-06 10:54:50 +0900

    Add hidden CSS
    입력값 받은 후 form 안 보이게 하기

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const loginInput = document.querySelector("#login-form input");
 
 function onLoginSubmit(event) {
   event.preventDefault(); //event의 기본동작을 막아주는 코드
-  console.log(loginInput.value); //콘솔창에 사용자가 input란에 입력한 값이 뜬다.
+  const username = loginInput.value; //사용자 입력값 받아서 변수에 저장
+  loginForm.classList.add("hidden");
 }
 
 loginForm.addEventListener("submit", onLoginSubmit); //submit event
